ee585010715459a8ad156ed59e59127d1b7e4ab6
Sandroisu <sandroisu1411@gmail.com>
2023-04-22 15:57:39 +0300

    исправления

--- User.php
+++ User.php
@@ -12,7 +12,6 @@ class User
 
     public function getById(int $id): ?Entity\User
     {
-        // Check if the entity has already been loaded
         if (isset($this->identityMap[$id])) {
             return $this->identityMap[$id];
         }
@@ -20,7 +19,6 @@ class User
         foreach ($this->getDataFromSource(['id' => $id]) as $user) {
             $entity = $this->createUser($user);
 
-            // Add the entity to the identity map
             $this->identityMap[$id] = $entity;
 
             return $entity;
@@ -60,7 +58,50 @@ class User
 
     private function getDataFromSource(array $search = [])
     {
-        // ...
 
+        $admin = ['id' => 1, 'title' => 'Super Admin', 'role' => 'admin'];
+        $user = ['id' => 1, 'title' => 'Main user', 'role' => 'user'];
+        $test = ['id' => 1, 'title' => 'For test needed', 'role' => 'test'];
+
+        $dataSource = [
+            [
+                'id' => 1,
+                'name' => 'Super Admin',
+                'login' => 'root',
+                'password' => '$2y$10$GnZbayyccTIDIT5nceez7u7z1u6K.znlEf9Jb19CLGK0NGbaorw8W', // 1234
+                'role' => $admin
+            ],
+            [
+                'id' => 2,
+                'name' => 'Doe John',
+                'login' => 'doejohn',
+                'password' => '$2y$10$j4DX.lEvkVLVt6PoAXr6VuomG3YfnssrW0GA8808Dy5ydwND/n8DW', // qwerty
+                'role' => $user
+            ],
+            [
+                'id' => 3,
+                'name' => 'Ivanov Ivan Ivanovich',
+                'login' => 'i**extends',
+                'password' => '$2y$10$TcQdU.qWG0s7XGeIqnhquOH/v3r2KKbes8bLIL6NFWpqfFn.cwWha', // PaSsWoRd
+                'role' => $user
+            ],
+            [
+                'id' => 4,
+                'name' => 'Test Testov Testovich',
+                'login' => 'testok',
+                'password' => '$2y$10$vQvuFc6vQQyon0IawbmUN.3cPBXmuaZYsVww5csFRLvLCLPTiYwMa', // testss
+                'role' => $test
+            ],
+        ];
+
+        if (!count($search)) {
+            return $dataSource;
+        }
+
+        $productFilter = function (array $dataSource) use ($search): bool {
+            return (bool) array_intersect($dataSource, $search);
+        };
+
+        return array_filter($dataSource, $productFilter);
     }
 }
